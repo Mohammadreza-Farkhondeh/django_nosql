c463133f734831ac341b2f9b71b75b5c99aa0f52
Mohammadreza-Farkhondeh <mohammadrfarkhondeh@gmail.com>
2023-08-02 21:56:32 +0330

    created view, template and utils for crud_page

--- file_upload/utils.py
+++ file_upload/utils.py
@@ -7,7 +7,7 @@ from .documents import FileDataDocument
 
 def process_uploaded_file(uploaded_file):
     """
-    return a list of dictionery from rows in data wheter its csv or json file
+    return a list of dictionary from rows in data wheter its csv or json file
     """
     data = []
     # full of security issues
@@ -23,7 +23,7 @@ def process_uploaded_file(uploaded_file):
 
 def save_to_mongodb(data):
     """
-    get a list of dictioneries, then save them each in mongodb
+    get a list of dictionaries, then save them each in mongodb
     """
     collection = mongo_db.collection
     collection.insert_many(data)
@@ -31,7 +31,38 @@ def save_to_mongodb(data):
 
 def save_to_elasticsearch(data):
     """
-    get a list of dictioneries, then save them each in elasticsearch
+    get a list of dictionaries, then save them each in elasticsearch
     """
     for row in data:
         FileDataDocument(meta={'id': row.get('id', None)}, data=row, uploaded_at=datetime.now()).save(index='filedata')
+
+
+def get_data_from_database(db_type):
+    """
+    retrieve list of rows from the database
+    """
+    if db_type == 'mongodb':
+        # Retrieve data from MongoDB
+        collection = mongo_db.collection
+        data = list(collection.find())
+    else:
+        # Retrieve data from Elasticsearch
+        s = FileDataDocument.search()
+        data = [hit.to_dict() for hit in s.execute()]
+
+    return data
+
+
+def save_to_database(data, db_type):
+    """
+    save a list of dictionaries to the database
+    """
+    if db_type == 'mongodb':
+        # Save data to MongoDB
+        collection = mongo_db.collection
+        collection.delete_many({})  # Clear the existing data
+        collection.insert_many(data)
+    else:
+        # Save data to Elasticsearch
+        for row in data:
+            FileDataDocument(meta={'id': row.get('id', None)}, data=row, uploaded_at=datetime.now()).save(index='filedata')
--- file_upload/views.py
+++ file_upload/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse
 
 from .utils import (process_uploaded_file,
                     save_to_elasticsearch,
-                    save_to_mongodb,)
+                    save_to_mongodb,
+                    save_to_database,
+                    get_data_from_database)
 
 
 def upload_file(request):
@@ -27,3 +29,52 @@ def upload_file(request):
         return HttpResponse("File uploaded successfully!")
 
     return render(request, 'upload_file.html')
+
+
+def crud_page(request):
+    """
+    shows a table with CRUD operations on the uploaded file data
+    db_type from "db_type" in request.session
+    get fields from first row of data
+    """
+    db_type = request.session.get('db_type')
+
+    if db_type not in ['mongodb', 'elasticsearch']:
+        return HttpResponse("Invalid database type")
+
+    data = get_data_from_database(db_type)
+    fields = list(data[0].keys())
+
+    if request.method == 'POST':
+        if 'create' in request.POST:
+            data = get_data_from_database(db_type)
+            new_row = {}  # Dictionary to store the new row data
+
+            # Iterate over the fields and get the new values from the form
+            for field in fields:
+                new_row[field] = request.POST.get(field, '')
+
+            data.append(new_row)
+            save_to_database(data, db_type)
+
+        elif 'update' in request.POST:
+            row_id = int(request.POST.get('row_id'))
+            updated_row = {}  # Dictionary to store the updated row data
+
+            # Iterate over the fields and get the updated values from the form
+            for field in fields:
+                updated_row[field] = request.POST.get(field, '')
+
+            data[row_id] = updated_row
+            save_to_database(data, db_type)
+
+        elif 'delete' in request.POST:
+            row_id = int(request.POST.get('row_id'))
+
+            data.pop(row_id)
+
+            save_to_database(data, db_type)
+
+    fields = list(data[0].keys())
+
+    return render(request, 'crud_page.html', {'data': data, 'fields': fields, 'db_type': db_type})

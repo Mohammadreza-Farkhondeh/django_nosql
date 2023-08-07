c6eb40749701a77e06a8685873f6aab87fd116fe
Mohammadreza-Farkhondeh <mohammadrfarkhondeh@gmail.com>
2023-08-07 15:31:49 +0330

    created a template library

--- django_nosql/settings.py
+++ django_nosql/settings.py
@@ -45,6 +45,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries':{
+                    'template_filters': 'django_nosql.template_filters',
+                }
         },
     },
 ]

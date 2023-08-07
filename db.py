c712badc4f11d58fc5aabca51b60f572c44c2615
Mohammadreza-Farkhondeh <mohammadrfarkhondeh@gmail.com>
2023-08-07 15:32:07 +0330

    fixed db.py

--- db.py
+++ db.py
@@ -1,21 +1,12 @@
 import pymongo
 from elasticsearch_dsl import connections
-from elasticsearch import Elasticsearch
 
-ELASTICSEARCH_CONNECTIONS = {
-    'default': {
-        'hosts': 'elasticsearch',
-        'port': 9200,
-    },
-}
-MONGO_CONNECTION_STRING = "mongodb://mongodb:27017"
 
 # Establish MongoDB connection
-mongo_client = pymongo.MongoClient(MONGO_CONNECTION_STRING)
+mongo_client = pymongo.MongoClient("localhost", 27017)
 mongo_db = mongo_client.database
 mongo_collection = mongo_db.collection
 
 # Establish Elasticsearch connection
-connections.configure(**ELASTICSEARCH_CONNECTIONS)
+connections.create_connection(alias='default', hosts=['localhost'], timeout=60)
 es = connections.get_connection(alias='default')
-print(type(es))
